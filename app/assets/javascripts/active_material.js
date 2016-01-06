ed1fb9750bd5f7806a24c9e5a20a68f8629f168c
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-01-06 16:51:53 -0500

    More minor design changes

--- app/assets/javascripts/active_material.js
+++ app/assets/javascripts/active_material.js
@@ -1,5 +1,6 @@
 //= require active_material/fonts
 //= require active_material/actions-toggle
 //= require active_material/drawer
+//= require active_material/sidebar
 
 document.querySelector('html').className += ' am-js'
---
+++ app/assets/javascripts/active_material/sidebar.js
@@ -0,0 +1,25 @@
+/**
+ * Sidebar
+ * Logic to produce a collapsing sidebar
+ */
+
+$(function() {
+  var $body = $("body")
+  var $sidebar = $("#sidebar")
+
+  if (!$sidebar.length) return
+
+  // .am-sidebar-enabled allows specific style hooks to create
+  // a collapseable, responsive navigation sidebar
+  $body.addClass("am-sidebar-enabled")
+
+  var $toggle = $("<span class='action_item am-sidebar-toggle-wrapper'><button class='am-sidebar-toggle'>Toggle Sidebar</button></span>")
+
+  $toggle.appendTo("#titlebar_right .action_items")
+
+  // Whenever the toggle is pressed, toggle an active class on the body
+  $toggle.click(function(event) {
+    event.preventDefault()
+    $body.toggleClass("am-sidebar-open")
+  })
+})
